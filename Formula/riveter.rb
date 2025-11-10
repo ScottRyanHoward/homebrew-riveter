a9d907225ed670bdd3e153a8755566a988ec9562
ScottRyanHoward <scottryanhoward@gmail.com>
2025-11-10 15:07:18 -0500

    Fix checksums for version 0.13.11

--- Formula/riveter.rb
+++ Formula/riveter.rb
@@ -6,18 +6,18 @@ class Riveter < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/ScottRyanHoward/riveter/releases/download/v0.13.11/riveter-0.13.11-macos-intel.tar.gz"
-    sha256 "PLACEHOLDER_CHECKSUM_MACOS_INTEL"
+    sha256 "4e36bab0578bd2c967cf8db5967ad1b09a596b74558d7027a8cd92b15acf232e"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/ScottRyanHoward/riveter/releases/download/v0.13.11/riveter-0.13.11-macos-arm64.tar.gz"
-    sha256 "PLACEHOLDER_CHECKSUM_MACOS_ARM64"
+    sha256 "0a4bd2afbe8a2bf9ac39506b6f46c71acd414465dec0547e00cf1fff396c7554"
   elsif OS.linux?
     url "https://github.com/ScottRyanHoward/riveter/releases/download/v0.13.11/riveter-0.13.11-linux-x86_64.tar.gz"
-    sha256 "PLACEHOLDER_CHECKSUM_LINUX_X86_64"
+    sha256 "8a188180dc911d4e0454e35ee1854818ec9218b75b50a2831a5ca177ca2619bd"
   end
 
   resource "rule_packs" do
     url "https://github.com/ScottRyanHoward/riveter/archive/v0.13.11.tar.gz"
-    sha256 "PLACEHOLDER_SOURCE_CHECKSUM"
+    sha256 "a4f623cd6cd28fb5024666ea47b50c4ae316218e5c1ce885b88252c69f1573cd"
   end
 
   def install
